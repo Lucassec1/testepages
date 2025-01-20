6552283ae30b0aefa2d5390c0d6b7d6d6aa5eaf0
EmanuelCicero <emanuelcicero40@gmail.com>
2025-01-20 12:39:47 -0300

    teste12

--- src/data/dadosEdicaoAtual.jsx
+++ src/data/dadosEdicaoAtual.jsx
@@ -1,5 +1,5 @@
 export const dadosEdicaoAtual = {
-  anoAtualDoEvento: "2026",
+  anoAtualDoEvento: "2025",
   dataDaRealizacaoDoWASHES: "xx e xx de xxxxx de 2026",
   // edicaoDoCongresso: "Como um evento satélite do 46° Congresso da Sociedade Brasileira de Computação (CSBC) xx e xx de xxxxx de 2026.",
 
