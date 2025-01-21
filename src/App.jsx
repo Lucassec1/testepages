060efd9599f1f50156187ea835b83c77095c71b8
lucassec1 <lucasemmanuelcs@gmail.com>
2025-01-21 14:51:33 -0300

    refactor: Update route paths and import statements for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import QuemSomos from './pages/QuemSomos';
+import Washes2016 from './pages/WASHES/Washes2016';
+import Washes2017 from './pages/WASHES/Washes2017';
+import Washes2018 from './pages/WASHES/Washes2018';
+import Washes2019 from './pages/WASHES/Washes2019';
+import Washes2020 from './pages/WASHES/Washes2020';
+import Washes2021 from './pages/WASHES/Washes2021';
+import Washes2022 from './pages/WASHES/Washes2022';
+import Washes2023 from './pages/WASHES/Washes2023';
+import Washes2024 from './pages/WASHES/Washes2024';
+import Washes2025 from './pages/WASHES/Washes2025';
+import Washes2026 from './pages/WASHES/Washes2026';
 import EdicoesAnteriores from './pages/EdicoesAnteriores';
 import DataWashes from './pages/DataWashes';
-import ScrollToTop from './components/ScrollTop';
-import {
-  Washes2016,
-  Washes2017,
-  Washes2018,
-  Washes2019,
-  Washes2020,
-  Washes2021,
-  Washes2022,
-  Washes2023,
-  Washes2024,
-  Washes2025,
-  Washes2026
-} from "../src/pages/WASHES/index";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <ScrollToTop />
       <div className="pt-14">
@@ -30,25 +28,25 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/quem-somos" element={<QuemSomos />} />
-          <Route path="/washes-2016" element={<Washes2016/>} />
-          <Route path="/washes-2017" element={<Washes2017/>} />
-          <Route path="/washes-2018" element={<Washes2018/>} />
-          <Route path="/washes-2019" element={<Washes2019/>} />
-          <Route path="/washes-2020" element={<Washes2020/>} />
-          <Route path="/washes-2021" element={<Washes2021/>} />
-          <Route path="/washes-2022" element={<Washes2022/>} />
-          <Route path="/washes-2023" element={<Washes2023/>} />
-          <Route path="/washes-2024" element={<Washes2024/>} />
-          <Route path="/washes-2025" element={<Washes2025/>} />
-          <Route path="/washes-2026" element={<Washes2026/>} />
+          <Route path="/washes-2016" element={<Washes2016 />} />
+          <Route path="/washes-2017" element={<Washes2017 />} />
+          <Route path="/washes-2018" element={<Washes2018 />} />
+          <Route path="/washes-2019" element={<Washes2019 />} />
+          <Route path="/washes-2020" element={<Washes2020 />} />
+          <Route path="/washes-2021" element={<Washes2021 />} />
+          <Route path="/washes-2022" element={<Washes2022 />} />
+          <Route path="/washes-2023" element={<Washes2023 />} />
+          <Route path="/washes-2024" element={<Washes2024 />} />
+          <Route path="/washes-2025" element={<Washes2025 />} />
+          <Route path="/washes-2026" element={<Washes2026 />} />
           <Route path="/edicoes-anteriores" element={<EdicoesAnteriores />} />
           <Route path="/datawashes" element={<DataWashes />} />
           {/* <Route path="*" element={<NoMatch />} /> */}
         </Routes>
       </div>
       <Footer />
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
