9d0ddc440e414f355e2b2f9d588967aafc89491f
lucassec1 <lucasemmanuelcs@gmail.com>
2025-01-21 15:00:19 -0300

    refactor: Remove unused banner image for Washes2026 route

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ export default {
         "banner-hero": "url(/assets/banners/banner-hero-home.svg)",
         "teste-foto" : "url(/assets/imgs/logo_washes.svg)",
         // Altere o caminho da nova foto do local do próximo evento abaixo
-        "banner-hero-WASHES2026":"url(/assets/banners/banner-hero-WASHES2026.jpg)",
         "banner-hero-WASHES2025":"url(/assets/banners/banner-hero-WASHES2025.jpg)",
         "banner-hero-WASHES2024":"url(/assets/banners/banner-hero-WASHES2024.jpg)",
         "banner-hero-WASHES2023":"url(/assets/banners/banner-hero-WASHES2023.jpg)",
